7fb05d942cf1726bbdad39fede07515bb2e2532c
Alex Anderson <andersonaj116@gmail.com>
2023-03-09 09:56:18 -0600

    Created paddle dynamic behavior

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -21,6 +21,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeLoseZone()
         kickBall()
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
    
     func resetGame(){
         //happens before each game starts
